ed34653e7b3c797274b247d117ce92b02496e3c3
Mike Lat <mikelat@gmail.com>
2014-04-04 22:20:11 -0700

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ SampleApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
